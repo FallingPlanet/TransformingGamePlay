36bfdd4a5758170dc1576ce34d372981fcde0278
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-03-27 21:35:05 -0400

    Uploaded more

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Example: Log a message when the document is fully loaded
+    console.log("Document fully loaded and parsed");
+});
